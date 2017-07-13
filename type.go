ee07e6bbbb7fc9cd2ecb8d6416ee0134fc255f34
Tim Cooper <tim.cooper@layeh.com>
2017-07-13 07:39:10 -0300

    change NewType error messages to be more user friendly

--- type.go
+++ type.go
@@ -18,12 +18,31 @@ func typeCall(L *lua.LState) int {
 	switch ref.Kind() {
 	case reflect.Chan:
 		buffer := L.OptInt(2, 0)
+
+		if buffer < 0 {
+			L.ArgError(2, "negative buffer size")
+		}
+		if ref.ChanDir() != reflect.BothDir {
+			L.RaiseError("unidirectional channel type")
+		}
+
 		value = reflect.MakeChan(ref, buffer)
 	case reflect.Map:
 		value = reflect.MakeMap(ref)
 	case reflect.Slice:
 		length := L.OptInt(2, 0)
 		capacity := L.OptInt(3, length)
+
+		if length < 0 {
+			L.ArgError(2, "negative length")
+		}
+		if capacity < 0 {
+			L.ArgError(3, "negative capacity")
+		}
+		if length > capacity {
+			L.RaiseError("length > capacity")
+		}
+
 		value = reflect.MakeSlice(ref, length, capacity)
 	default:
 		value = reflect.New(ref)
