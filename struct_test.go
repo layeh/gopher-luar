layeh/gopher-luar

commit 3c25e10c9d807f7d47647fd7d04d6f3360550a85

support unexported anonymous fields

cache.go
@@ -30,7 +30,7 @@ func collectFields(vtype reflect.Type, current []int) map[string]reflect.StructF
 
 	for i, n := 0, vtype.NumField(); i < n; i++ {
 		field := vtype.Field(i)
-		if field.PkgPath != "" {
+		if field.PkgPath != "" && !field.Anonymous {
 			continue
 		}
 

struct_test.go
@@ -367,3 +367,28 @@ func Test_ambiguous_field2(t *testing.T) {
 		t.Errorf("expected Test_nested_child2.Name to be empty")
 	}
 }
+
+type test_unexport_anonymous_child struct {
+	Name string
+}
+
+type Test_struct_unexport_anonymous_parent struct {
+	test_unexport_anonymous_child
+}
+
+func Test_struct_unexport_anonymous_field(t *testing.T) {
+	L := lua.NewState()
+	defer L.Close()
+
+	n := &Test_struct_unexport_anonymous_parent{}
+
+	L.SetGlobal("c", New(L, n))
+	testReturn(t, L, `
+		c.Name = "Tim"
+		return c.Name
+	`, "Tim")
+
+	if n.Name != "Tim" {
+		t.Errorf("expected Name to be set to `Tim`")
+	}
+}

commit 9951531f780ed6710cce8773f03a24880f6f1bed

fix unexported anonymous field being included in struct field list
bug introduced in 3c25e10c9d807f7d47647fd7d04d6f3360550a85

cache.go
@@ -30,12 +30,11 @@ func collectFields(vtype reflect.Type, current []int) map[string]reflect.StructF
 
 	for i, n := 0, vtype.NumField(); i < n; i++ {
 		field := vtype.Field(i)
-		if field.PkgPath != "" && !field.Anonymous {
-			continue
-		}
 
-		field.Index = append(current[:len(current):len(current)], i)
-		m[field.Name] = field
+		if field.PkgPath == "" {
+			field.Index = append(current[:len(current):len(current)], i)
+			m[field.Name] = field
+		}
 
 		if field.Anonymous {
 			t := field.Type

struct_test.go
@@ -391,4 +391,11 @@ func Test_struct_unexport_anonymous_field(t *testing.T) {
 	if n.Name != "Tim" {
 		t.Errorf("expected Name to be set to `Tim`")
 	}
+
+	// Ensure test_unexport_anonymous_child was not captured
+	mt := MT(L, *n)
+	fields := mt.RawGetString("fields").(*lua.LTable)
+	if field := fields.RawGetString("test_unexport_anonymous_child"); field != lua.LNil {
+		t.Errorf("expected test_unexport_anonymous_child to be nil, got %v", field)
+	}
 }